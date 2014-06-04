90f981e60dc7fc7e85c5dfc6a13cc0062ec5efef
joemills82 <github@joemills.co.uk>
2014-06-04 12:26:40 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -150,9 +150,9 @@
 										height: 200,
 										renderer: 'line',
 										//min: parseFloat(datastream.min_value) - .5*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										min: parseFloat(datastream.min_value),
+										min: 10,
 										//max: parseFloat(datastream.max_value) + .5*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value), 
+										max: 30, 
 										padding: {
 											top: 0.02,
 											right: 0.02,
